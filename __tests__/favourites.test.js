35a6968e3a7ff212a924828aa98303be65404eee
Saad Essam <development@it-synergy.nl>
2022-09-17 19:05:19 +0200

    remove commented code

--- __tests__/favourites.test.js
+++ __tests__/favourites.test.js
@@ -4,18 +4,8 @@ import Favourites from '../pages/favourites'
 describe('Favourites component', () => {
   test('renders "No products found" if no product exists', () => {
     render(<Favourites />)
-
-    const cardElements = screen.getAllByTestId('card-container')
-    const x = expect(cardElements).not.toHaveLength(0)
-    console.log(cardElements, x)
     const noProductFoundText = screen.getByText(/No products found/i, { exact: false })
     expect(noProductFoundText).toBeInTheDocument()
   })
 
-
-  // test('Does not render "No products found" if a product exists', () => {
-  //   render(<Favourites />)
-  //   const noProductFoundText = screen.queryByText(/No products found/i, { exact: false })
-  //   expect(noProductFoundText).toBeNull()
-  // })
 })
